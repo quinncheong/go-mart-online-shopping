c6b95ca35dc0787933ad3ac3f61c01270944b10d
Quinn Cheong <quinncheong@hotmail.com>
2023-03-20 23:17:32 +0800

    Init item Microservice

---
+++ api/item/item.Dockerfile
@@ -0,0 +1,6 @@
+FROM python:3-slim
+WORKDIR /usr/src/app
+COPY requirements.txt ./
+RUN pip install --no-cache-dir -r requirements.txt
+COPY ./item.py ./item_controller.py ./
+CMD ["python", "./item.py"]
---
+++ api/item/item.py
@@ -0,0 +1,44 @@
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+import item_controller
+
+app = Flask(__name__)
+CORS(app)
+
+@app.route('/')
+def hello():
+    return "Item connected"
+
+
+@app.route('/get-all-items', methods=['POST'])
+def get_all_items():
+    data = request.get_json()
+
+    # esk refers to {"item_name": ""}
+    if 'esk' in data:
+        esk = data['esk']
+        print('hello')
+        print(esk)
+        res = item_controller.get_all_items(esk)["Items"]
+    else:
+        res = item_controller.get_all_items()["Items"]
+
+    return res if res else "No items found/left"
+
+
+@app.route('/get-item', methods=['POST'])
+def get_item():
+    data = request.get_json()
+    key = data['key']
+    res = item_controller.get_item(key)
+    return res if res else "No items found/left"
+
+
+@app.route('/get-num-items', methods=['GET'])
+def get_num_items():
+    res = item_controller.get_num_items()
+    return str(res) if res else "0"
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5003, debug=True)
