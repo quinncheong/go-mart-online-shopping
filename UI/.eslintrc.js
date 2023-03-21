c495a637800ff4aab03387b542458504a06e2f66
Quinn Cheong <quinncheong@hotmail.com>
2023-03-21 12:31:24 +0800

    Disable some lint configs

--- UI/.eslintrc.js
+++ UI/.eslintrc.js
@@ -6,5 +6,6 @@ module.exports = {
   extends: ["plugin:vue/vue3-essential", "eslint:recommended"],
   rules: {
     "vue/multi-word-component-names": "off",
+    "vue/no-unused-components": "off",
   },
 };
--- UI/vite.config.js
+++ UI/vite.config.js
@@ -8,44 +8,54 @@ import { fileURLToPath, URL } from "node:url";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue({
-      template: { transformAssetUrls },
-    }),
-    // https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vite-plugin
-    vuetify({
-      autoImport: true,
-      styles: {
-        configFile: "src/styles/settings.scss",
-      },
-    }),
-  ],
-  define: { "process.env": {} },
-  resolve: {
-    alias: {
-      "@": fileURLToPath(new URL("./src", import.meta.url)),
-    },
-    extensions: [".js", ".json", ".jsx", ".mjs", ".ts", ".tsx", ".vue"],
-  },
-  server: {
-    port: 3000,
-    proxy: {
-      "^/api": {
-        // target: "http://13.251.16.251:5003",
-        target: "http://localhost:5003",
-        changeOrigin: true,
-        pathRewrite: {
-          "^/api": "",
-        },
-      },
-      "^/po": {
-        // target: "http://13.251.105.185:5100",
-        target: "http://localhost:5002",
-        changeOrigin: true,
-        pathRewrite: {
-          "^/po": "",
-        },
-      },
-    },
-  },
+	plugins: [
+		vue({
+			template: { transformAssetUrls },
+		}),
+		// https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vite-plugin
+		vuetify({
+			autoImport: true,
+			styles: {
+				configFile: "src/styles/settings.scss",
+			},
+		}),
+	],
+	define: { "process.env": {} },
+	resolve: {
+		alias: {
+			"@": fileURLToPath(new URL("./src", import.meta.url)),
+		},
+		extensions: [
+			".js",
+			".json",
+			".jsx",
+			".mjs",
+			".ts",
+			".tsx",
+			".vue",
+			".jpg",
+			".png",
+		],
+	},
+	server: {
+		port: 3000,
+		proxy: {
+			"^/api": {
+				// target: "http://13.251.16.251:5003",
+				target: "http://localhost:5003",
+				changeOrigin: true,
+				pathRewrite: {
+					"^/api": "",
+				},
+			},
+			"^/po": {
+				// target: "http://13.251.105.185:5100",
+				target: "http://localhost:5002",
+				changeOrigin: true,
+				pathRewrite: {
+					"^/po": "",
+				},
+			},
+		},
+	},
 });
