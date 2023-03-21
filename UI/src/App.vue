814bbbdf750c1954e089781f89271de83305c4d0
Quinn Cheong <quinncheong@hotmail.com>
2023-03-21 12:31:48 +0800

    Add placeholders and css

--- UI/src/App.vue
+++ UI/src/App.vue
@@ -1,7 +1,27 @@
 <template>
-  <router-view />
+	<v-app style="background: #faf8f7">
+		<NavigationBar />
+		<div class="container pa-5 mt-5">
+			<v-content>
+				<router-view />
+			</v-content>
+		</div>
+	</v-app>
 </template>
 
 <script setup>
-  //
+import HelloWorld from "@/components/HelloWorld.vue";
+import NavigationBar from "@/components/NavigationBar.vue";
+import Items from "@/components/Items.vue";
+</script>
+
+<script>
+export default {
+	name: "App",
+	components: {
+		NavigationBar,
+		HelloWorld,
+		Items,
+	},
+};
 </script>
