da0c19ffa6bf72756118ac2c7dde13c8b8204299
Quinn Cheong <quinncheong@hotmail.com>
2023-03-21 17:40:09 +0800

    Add connection to dynamoDB for item table

--- api/item/item.py
+++ api/item/item.py
@@ -1,23 +1,47 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
+from dotenv import load_dotenv
 import item_controller
+
+load_dotenv("./.env")
 
 app = Flask(__name__)
 CORS(app)
 
-@app.route('/')
+
+@app.route("/")
 def hello():
+    """
+    Health Check Endpoint
+    """
     return "Item connected"
 
 
-@app.route('/get-all-items', methods=['POST'])
+@app.route("/item/all")
+def get_items():
+    """
+    This function gets all items from the database
+    """
+    return item_controller.get_all_items()
+
+
+@app.get("/item/<item_id>")
+def get_item_by_id(item_id: str = None):
+    """
+    This function a specific item from the database,
+    querying by item_id
+    """
+    return item_controller.get_item(item_id)
+
+
+@app.route("/get-all-items", methods=["POST"])
 def get_all_items():
     data = request.get_json()
 
     # esk refers to {"item_name": ""}
-    if 'esk' in data:
-        esk = data['esk']
-        print('hello')
+    if "esk" in data:
+        esk = data["esk"]
+        print("hello")
         print(esk)
         res = item_controller.get_all_items(esk)["Items"]
     else:
@@ -26,19 +50,19 @@ def get_all_items():
     return res if res else "No items found/left"
 
 
-@app.route('/get-item', methods=['POST'])
+@app.route("/get-item", methods=["POST"])
 def get_item():
     data = request.get_json()
-    key = data['key']
+    key = data["key"]
     res = item_controller.get_item(key)
     return res if res else "No items found/left"
 
 
-@app.route('/get-num-items', methods=['GET'])
+@app.route("/get-num-items", methods=["GET"])
 def get_num_items():
     res = item_controller.get_num_items()
     return str(res) if res else "0"
 
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5003, debug=True)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5003, debug=True)
--- api/item/item_controller.py
+++ api/item/item_controller.py
@@ -1,20 +1,22 @@
 import boto3
 import os
 
+REGION = os.environ.get("REGION")
+ACCESS_KEY = os.environ.get("ACCESS_KEY")
+SECRET_KEY = os.environ.get("SECRET_KEY")
+
 dynamodb = boto3.resource(
-  'dynamodb',
-  region_name=os.environ.get('REGION'),
-  aws_access_key_id=os.environ.get('ACCESS_KEY'),
-  aws_secret_access_key=os.environ.get('SECRET_KEY'))
+    "dynamodb",
+    region_name=REGION,
+    aws_access_key_id=ACCESS_KEY,
+    aws_secret_access_key=SECRET_KEY,
+)
 
 connection = boto3.client(
-  'dynamodb',
-  os.environ.get('REGION'),
-  aws_access_key_id=os.environ.get('ACCESS_KEY'),
-  aws_secret_access_key=os.environ.get('SECRET_KEY'))
+    "dynamodb", REGION, aws_access_key_id=ACCESS_KEY, aws_secret_access_key=SECRET_KEY
+)
 
-
-item_table = dynamodb.Table('item')
+item_table = dynamodb.Table("itemDetails")
 
 
 def get_all_items(esk={}):
@@ -23,9 +25,14 @@ def get_all_items(esk={}):
     return item_table.scan(Limit=2)
 
 
-def get_item(key={}):
-    return item_table.get_item(Key=key)
+def get_item(item_id=None):
+    if not item_id:
+        return
+
+    key = {"id": str(item_id)}
+    res = item_table.get_item(Key=key)
+    return res["Item"] if res else None
 
 
 def get_num_items():
-    return connection.describe_table(TableName='item')['Table']['ItemCount']
+    return connection.describe_table(TableName="item")["Table"]["ItemCount"]
