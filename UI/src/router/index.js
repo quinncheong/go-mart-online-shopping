b738df058293c5bf5ce0202314ff2323270da9f6
Quinn Cheong <quinncheong@hotmail.com>
2023-03-21 12:32:03 +0800

    Reroute certain vue components

--- UI/src/router/index.js
+++ UI/src/router/index.js
@@ -2,51 +2,41 @@
 import { createRouter, createWebHistory } from "vue-router";
 
 const routes = [
-  {
-    path: "/",
-    component: () => import("@/layouts/default/Default.vue"),
-    children: [
-      {
-        path: "/item",
-        name: "Item",
-        component: () => import("../views/Item.vue"),
-      },
-      {
-        path: "/cart",
-        name: "Cart",
-        component: () => import("../components/Cart.vue"),
-      },
-      {
-        path: "/checkout",
-        name: "Checkout",
-        component: () => import("../views/Checkout.vue"),
-      },
-      {
-        path: "/about",
-        name: "About",
-        component: () => import("../views/About.vue"),
-      },
-      {
-        path: "/hello",
-        name: "Hello",
-        component: () => import("../views/Hello.vue"),
-      },
-      {
-        path: "",
-        name: "Home",
-        // route level code-splitting
-        // this generates a separate chunk (about.[hash].js) for this route
-        // which is lazy-loaded when the route is visited.
-        component: () =>
-          import(/* webpackChunkName: "home" */ "@/views/Home.vue"),
-      },
-    ],
-  },
+	{
+		path: "/",
+		component: () => import("@/layouts/default/Default.vue"),
+		children: [
+			{
+				path: "/item",
+				name: "Item",
+				component: () => import("@/views/ItemPage.vue"),
+			},
+			{
+				path: "/about",
+				name: "About",
+				component: () => import("@/views/About.vue"),
+			},
+			{
+				path: "/hello",
+				name: "Hello",
+				component: () => import("@/views/Hello.vue"),
+			},
+			{
+				path: "",
+				name: "Home",
+				// route level code-splitting
+				// this generates a separate chunk (about.[hash].js) for this route
+				// which is lazy-loaded when the route is visited.
+				component: () =>
+					import(/* webpackChunkName: "home" */ "@/views/Home.vue"),
+			},
+		],
+	},
 ];
 
 const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
-  routes,
+	history: createWebHistory(process.env.BASE_URL),
+	routes,
 });
 
 export default router;
