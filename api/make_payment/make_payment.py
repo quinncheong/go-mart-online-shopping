069b0684b6ab9e941d38f021594fec93c7bd7ebb
Quinn Cheong <quinncheong@hotmail.com>
2023-03-22 22:04:56 +0800

    Reorder stripe folders

--- api/make_payment/make_payment.py
+++ api/make_payment/make_payment.py
@@ -11,10 +11,14 @@ from os import environ
 app = Flask(__name__)
 CORS(app)
 
-payment_URL = environ.get('payment_URL') or "http://localhost:5005/pay"
-read_booking_URL = environ.get('read_booking_URL') or"http://localhost:5002/read_booking"
-update_booking_URL = environ.get('update_booking_URL') or "http://localhost:5002/update_booking"
-email_URL = environ.get('email_URL') or "http://localhost:5008/sendVerificationEmail"
+payment_URL = environ.get("payment_URL") or "http://localhost:5005/pay"
+read_booking_URL = (
+    environ.get("read_booking_URL") or "http://localhost:5002/read_booking"
+)
+update_booking_URL = (
+    environ.get("update_booking_URL") or "http://localhost:5002/update_booking"
+)
+email_URL = environ.get("email_URL") or "http://localhost:5008/sendVerificationEmail"
 
 
 # @app.route("/getBookingInfo/", methods=['GET'])
@@ -26,35 +30,34 @@ email_URL = environ.get('email_URL') or "http://localhost:5008/sendVerificationE
 #     return bookingInfo
 
 
-@app.route("/new_payment", methods=['POST'])
+@app.route("/new_payment", methods=["POST"])
 def new_payment():
     # retrieve post request from stripe payment intent form
     data = request.get_json()
     app.logger.info(data)
-    if ('order_id' in data.keys()):
-        order_id = data['order_id']
+    if "order_id" in data.keys():
+        order_id = data["order_id"]
 
-        data['amount'] = int(float(data['amount']) * 100)
+        data["amount"] = int(float(data["amount"]) * 100)
         # get booking
         # bookingInfo = invoke_http(read_booking_URL + '?booking_id=' + booking_id, method='GET')
         # app.logger.info(bookingInfo)
-        
+
         # dummy to input payment price
         # data['price'] = int(float(bookingInfo['price']) * 100)
 
-
     # call stripe wrapper microservice
-    
+
     app.logger.info(data)
-    payment_intent = invoke_http(payment_URL, method='POST', json=data)
+    payment_intent = invoke_http(payment_URL, method="POST", json=data)
     # r = requests.request("POST", payment_URL, json = data)
     app.logger.info(payment_intent)
 
-    if ('success' in payment_intent.keys()):
+    if "success" in payment_intent.keys():
         success_data = {
-            'order_id': payment_intent['order_id'],
-            'payment_status': "Paid",
-            'payment_id': payment_intent['payment_id']
+            "order_id": payment_intent["order_id"],
+            "payment_status": "Paid",
+            "payment_id": payment_intent["payment_id"],
         }
         app.logger.info(success_data)
         # update_paymentstatus = invoke_http(
@@ -79,8 +82,6 @@ def new_payment():
     #     amqp_setup.channel.basic_publish(exchange=amqp_setup.exchangename, routing_key="booking.email",
     #                                     body=json.dumps(email_data),properties=pika.BasicProperties(delivery_mode=2))
 
-
-
     # # invoke error log if encounter code 500 error from stripe_wrapper (Invalid Status)
     # if 'error' in payment_intent.keys() or ('code' in payment_intent.keys() and payment_intent['code'] == 500):
     #     amqp_setup.channel.basic_publish(exchange=amqp_setup.exchangename, routing_key="booking.error",
@@ -89,5 +90,5 @@ def new_payment():
     return payment_intent
 
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8000, debug=True)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5005, debug=True)
--- api/stripe_wrapper/stripe_wrapper.py
+++ api/stripe_wrapper/stripe_wrapper.py
@@ -8,7 +8,7 @@ import json
 import stripe
 
 # This is your test secret API key.
-stripe.api_key = 'sk_test_51KegmTDlqIwRfGLSQlbxGkVbDTOlUTPyXZd6vFmOFGtlctJbEW1nw6FZIosaUu8jTyvSCRCrgrdha4fuVNX4E8ta00aua1NuKC'
+stripe.api_key = "sk_test_51KegmTDlqIwRfGLSQlbxGkVbDTOlUTPyXZd6vFmOFGtlctJbEW1nw6FZIosaUu8jTyvSCRCrgrdha4fuVNX4E8ta00aua1NuKC"
 
 
 app = Flask(__name__)
@@ -16,28 +16,28 @@ CORS(app)
 
 
 # AJAX endpoint when `/pay` is called from client
-@app.route('/pay', methods=['POST'])
+@app.route("/pay", methods=["POST"])
 def pay():
     data = request.get_json()
     intent = None
 
     try:
-        if 'payment_method_id' in data:
+        if "payment_method_id" in data:
             # Create the PaymentIntent
             intent = stripe.PaymentIntent.create(
-                payment_method=data['payment_method_id'],
-                amount=data['amount'],
-                currency='sgd',
-                confirmation_method='manual',
+                payment_method=data["payment_method_id"],
+                amount=data["amount"],
+                currency="sgd",
+                confirmation_method="manual",
                 confirm=True,
-                description=data['order_id'],
+                description=data["order_id"],
             )
-        elif 'payment_intent_id' in data:
-            intent = stripe.PaymentIntent.confirm(data['payment_intent_id'])
+        elif "payment_intent_id" in data:
+            intent = stripe.PaymentIntent.confirm(data["payment_intent_id"])
     except stripe.error.CardError as e:
         # Display error on client
         app.logger.info(intent)
-        return json.dumps({'error': e.user_message}), 200
+        return json.dumps({"error": e.user_message}), 200
 
     return generate_response(intent)
 
@@ -45,27 +45,38 @@ def pay():
 def generate_response(intent):
     # Note that if your API version is before 2019-02-11, 'requires_action'
     # appears as 'requires_source_action'.
-    if intent.status == 'requires_action' and intent.next_action.type == 'use_stripe_sdk':
+    if (
+        intent.status == "requires_action"
+        and intent.next_action.type == "use_stripe_sdk"
+    ):
         # Tell the client to handle the action
-        return json.dumps({
-            'requires_action': True,
-            'payment_intent_client_secret': intent.client_secret,
-        }), 200
-    elif intent.status == 'succeeded':
+        return (
+            json.dumps(
+                {
+                    "requires_action": True,
+                    "payment_intent_client_secret": intent.client_secret,
+                }
+            ),
+            200,
+        )
+    elif intent.status == "succeeded":
         # The payment didn’t need any additional actions and completed!
         # Handle post-payment fulfillment
         app.logger.info(intent)
-        return json.dumps(
-            {
-                'success': True,
-                'order_id': intent['description'],
-                'payment_id': intent['charges']['data'][0]['id'],
-            }
-        ), 200
+        return (
+            json.dumps(
+                {
+                    "success": True,
+                    "order_id": intent["description"],
+                    "payment_id": intent["charges"]["data"][0]["id"],
+                }
+            ),
+            200,
+        )
     else:
         # Invalid status from stripe
         app.logger.info(intent)
-        return json.dumps({'error': 500, 'data': intent}), 500
+        return json.dumps({"error": 500, "data": intent}), 500
 
 
 # @app.route("/refund", methods=['POST'])
@@ -84,5 +95,5 @@ def generate_response(intent):
 #         return jsonify(error=str(e)), 403
 
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5005, debug=True)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5006, debug=True)
