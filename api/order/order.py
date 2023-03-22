11c796ae7bad742952db6141002462893b5c950b
Quinn Cheong <quinncheong@hotmail.com>
2023-03-23 00:12:19 +0800

    Add order service main functionality

--- api/order/order.py
+++ api/order/order.py
@@ -6,48 +6,62 @@ import order_controller
 app = Flask(__name__)
 CORS(app)
 
+
 @app.route("/")
 def hello():
-  return 'Order connected'
+    """
+    Health Check Endpoint
+    """
+    return "Order connected"
 
 
-@app.route('/get-all-orders')
+@app.route("/order/all")
 def get_all_orders():
-  res = order_controller.get_all_orders()["Items"]
-  return res
+    """
+    This function gets all orders from the database
+    """
+    return order_controller.get_all_orders()
 
 
-@app.route('/get-orders-by-email', methods=['POST'])
-def get_orders_by_email():
-  data = request.get_json()
-  email = data['email']
-
-  res = order_controller.get_orders_by_email(email)
-  return res["Items"]
-
-
-@app.route('/add-order', methods=['POST'])
-def add_order():
-  data = json.loads(request.get_json(force=True))
-  email = data['email']
-  items_dict = data['items']
-
-  res = order_controller.add_order(email, items_dict)
-  if res["ResponseMetadata"]["HTTPStatusCode"] in range(200, 300):
-    return jsonify(
-      {
-        "code": res["ResponseMetadata"]["HTTPStatusCode"],
-        "message": "Order has been added successfully"
-      }
-    ), res["ResponseMetadata"]["HTTPStatusCode"]
-
-  return jsonify(
-    {
-      "code": res["ResponseMetadata"]["HTTPStatusCode"],
-      "message": "Bad request"
-    }
-  ), res["ResponseMetadata"]["HTTPStatusCode"]
+@app.route("/order/<order_id>")
+def get_order_by_id(order_id: str = None):
+    """
+    This function gets a specific order from the database,
+    querying by order_id
+    """
+    return order_controller.get_order(order_id)
 
 
-if __name__ == '__main__':
-  app.run(host='0.0.0.0', port=5002, debug=True)
+@app.route("/order", methods=["POST"])
+def create_order():
+    """
+    This function creates a new order in the database
+    """
+    data = json.loads(request.get_json(force=True))
+    items_dict = data["items"]
+
+    res = order_controller.add_order(items_dict)
+    if res["ResponseMetadata"]["HTTPStatusCode"] in range(200, 300):
+        return (
+            jsonify(
+                {
+                    "code": res["ResponseMetadata"]["HTTPStatusCode"],
+                    "message": "Order has been added successfully",
+                }
+            ),
+            res["ResponseMetadata"]["HTTPStatusCode"],
+        )
+
+    return (
+        jsonify(
+            {
+                "code": res["ResponseMetadata"]["HTTPStatusCode"],
+                "message": "Bad request",
+            }
+        ),
+        res["ResponseMetadata"]["HTTPStatusCode"],
+    )
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5007, debug=True)
--- api/order/order_controller.py
+++ api/order/order_controller.py
@@ -3,40 +3,55 @@ from boto3.dynamodb.conditions import Key
 import os
 from datetime import datetime
 
+REGION = os.environ.get("REGION")
+ACCESS_KEY = os.environ.get("ACCESS_KEY")
+SECRET_KEY = os.environ.get("SECRET_KEY")
+
 dynamodb = boto3.resource(
-  'dynamodb', 
-  region_name=os.environ.get('REGION'), 
-  aws_access_key_id=os.environ.get('ACCESS_KEY'), 
-  aws_secret_access_key=os.environ.get('SECRET_KEY'))
+    "dynamodb",
+    region_name=REGION,
+    aws_access_key_id=ACCESS_KEY,
+    aws_secret_access_key=SECRET_KEY,
+)
 
-order_table = dynamodb.Table('order')
+order_table = dynamodb.Table("order")
 
-def get_all_orders():
-  return order_table.scan()
+
+def get_all_orders(esk={}):
+    if esk:
+        return order_table.scan(Limit=2, ExclusiveStartKey=esk)
+    return order_table.scan(Limit=2)
+
+
+def get_order(order_id=None):
+    if not order_id:
+        return
+
+    key = {"id": int(order_id)}
+    res = order_table.get_item(Key=key)
+    return res["Item"] if res else None
+
 
 def get_orders_by_email(email):
-  res = order_table.query(
-    KeyConditionExpression=Key('user_email').eq(email)
-  )
-  return res
+    res = order_table.query(KeyConditionExpression=Key("user_email").eq(email))
+    return res
+
 
 def add_order(email, items_dict):
-  date = datetime.now().strftime('%b %d, %Y %H:%M:%S')
+    date = datetime.now().strftime("%b %d, %Y %H:%M:%S")
 
-  order_id = datetime.now().strftime('%s')
+    order_id = datetime.now().strftime("%s")
 
-  with order_table.batch_writer() as batch:
-    for (item_name, item_quantity) in items_dict.items():
-      batch.put_item(
-        Item = {
-          'user_email': email,
-          'order_date': str(date),
-          'item_name': item_name,
-          'item_quantity': item_quantity,
-          'order_id': order_id
-        }
-      )
-  res = order_table.query(
-    KeyConditionExpression=Key('order_id').eq(order_id)
-  )
-  return res
+    with order_table.batch_writer() as batch:
+        for item_name, item_quantity in items_dict.items():
+            batch.put_item(
+                Item={
+                    "user_email": email,
+                    "order_date": str(date),
+                    "item_name": item_name,
+                    "item_quantity": item_quantity,
+                    "order_id": order_id,
+                }
+            )
+    res = order_table.query(KeyConditionExpression=Key("order_id").eq(order_id))
+    return res
