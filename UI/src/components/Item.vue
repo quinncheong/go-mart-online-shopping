dab321439446219705dc80b18d25fae02fcdcf19
Ranon Sew <ranonsew@gmail.com>
2023-03-29 04:14:27 +0800

    Update Item.vue
    changed the payload to match the microservice
    
    changed the data to match more closely to each/one-another
    
    changed the availableStock to computed, and updated the checking thing to >= 1
    
    added class="bg-black" to the add to cart button (wasn't showing up on my end otherwise)

--- UI/src/components/Item.vue
+++ UI/src/components/Item.vue
@@ -5,7 +5,7 @@
 				<v-row class="ma-5">
 					<v-col cols="3" class="d-flex flex-column">
 						<v-img
-							:src="item.item_image"
+							:src="item_image"
 							position="left"
 							contain
 							max-height="500px"
@@ -14,17 +14,17 @@
 					</v-col>
 					<v-col class="d-flex flex-column">
 						<v-card-title class="bold-30">
-							{{ item.item_name }}
+							{{ item_name }}
 						</v-card-title>
 						<v-card-subtitle class="text-left medium-20 mt-2 mb-n4">
-							Platform: {{ item.item_platform }}
+							Platform: {{ item_platform }}
 						</v-card-subtitle>
 
 						<v-spacer></v-spacer>
 
 						<v-card-subtitle class="text-left medium-20 mt-2 mb-n4">
 							<span class="ml-n1">
-								${{ item.item_price }}
+								${{ item_price }}
 							</span>
 						</v-card-subtitle>
 
@@ -37,20 +37,20 @@
 						<v-spacer></v-spacer>
 
 						<v-card-subtitle class="text-left medium-15 mt-n5">
-							{{ item.item_desc }}
+							{{ item_desc }}
 						</v-card-subtitle>
 
 						<v-spacer></v-spacer>
 
 						<v-card-actions class="ml-auto">
-							<v-btn color="#353535" rounded v-if="availableStock()">
+							<v-btn class="bg-black" rounded v-if="availableStock">
 								<v-card-subtitle class="white-15" @click="handleAddToCart()">
 									Add to Cart
 								</v-card-subtitle>
 								<v-icon color="white">mdi-plus</v-icon>
 							</v-btn>
 							<v-card>
-								<v-card-subtitle v-if="!availableStock()" rounded class="mx-auto">
+								<v-card-subtitle v-if="!availableStock" rounded class="mx-auto">
 									Out of Stock
 								</v-card-subtitle>
 							</v-card>
@@ -70,44 +70,51 @@ export default {
 	name: "Item",
 	data() {
 		return {
+			id: this.$route.query.id,
 			item_name: this.$route.query.item_name,
-			item: {
-				item_name: "",
-				item_price: 0,
-				item_desc: "",
-				item_image: placeholder,
-				item_platform: "",
-				item_stock: 0,
-			},
+			item_price: 100,
+			item_desc: "",
+			item_image: "",
+			item_platform: "",
+			item_stock: 100,
 		};
 	},
 	methods: {
 		getItem() {
-			const payload = { key: { item_name: this.item_name } };
+			const payload = { key: this.id }; // should be in the form --> { key: 63 }
 			const path = `${process.env.ITEM_BASEURL}/get-item`;
 			axios
 				.post(path, payload)
 				.then((res) => {
 					console.log("res data item: ", res)
-					this.item = res.data.Item;
+					const item = res.data.Item;
+					this.item_price = item.Price
+					this.item_desc = "Placeholder Description";
+					this.item_image = item.ImageLink;
+					this.item_platform = "Placeholder Platform";
+					this.item_stock = 100;
 				})
 				.catch((error) => {
-					this.item = {
-						item_name: "Test Item",
-						item_price: 100,
-						item_desc: "Tesitng Desc sdfjsdfhsdjkfhsdhf",
-						item_image: placeholder,
-						item_platform: "hsdfhjsdksdjlsj",
-						item_stock: 10000,
-					};
+					this.item_name = "Test Item";
+					this.item_price = 100;
+					this.item_desc = "Tesitng Desc sdfjsdfhsdjkfhsdhf";
+					this.item_image = placeholder;
+					this.item_platform = "hsdfhjsdksdjlsj";
+					this.item_stock = 10000;
 					console.error(error);
 				});
 		},
 		handleAddToCart() {
-			this.$store.dispatch("addItemToCart", this.item);
+			this.$store.dispatch("addItemToCart", {
+				id: this.id, item_name: this.item_name, item_price: this.item_price,
+				item_desc: this.item_desc, item_image: this.item_image,
+				item_platform: this.item_platform, item_stock: this.item_stock
+			});
 		},
+	},
+	computed: {
 		availableStock() {
-			return this.item_stock > 1 ? true : false;
+			return this.item_stock >= 1 ? true : false;
 		},
 	},
 	created() {
