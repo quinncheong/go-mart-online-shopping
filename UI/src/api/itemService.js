92da083997e3b6f146fae6d6ebdad32cc8582f27
Quinn Cheong <quinncheong@hotmail.com>
2023-03-31 02:47:05 +0800

    Add origin to fix cors error

--- UI/src/api/itemService.js
+++ UI/src/api/itemService.js
@@ -3,6 +3,9 @@ import axios from "axios";
 const { ITEM_BASEURL } = process.env;
 const ITEM_URL = `${ITEM_BASEURL}/v1/item`;
 
+// Fix CORs issue with axios
+axios.defaults.headers.common["origin"] = "https://api.gomartttt.store";
+
 export const getAllItems = async () => {
 	const response = await axios.get(`${ITEM_URL}/all`);
 	console.info(response);
