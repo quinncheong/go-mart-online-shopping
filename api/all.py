df52ce3870dba04bf75c47bbe43c8dc0e4d8970a
Ranon Sew <ranonsew@gmail.com>
2023-04-01 02:44:56 +0800

    Update all.py

--- api/all.py
+++ api/all.py
@@ -1,14 +1,11 @@
 import os
 from threading import Thread
 
-def run(x):
-  os.system(f"python ./{x}/{name}.py")
-
 if __name__ == "__main__":
-  Thread(target=lambda: os.system(f"python ./email/email.py")).start()
-  Thread(target=lambda: os.system(f"python ./error/error.py")).start()
-  Thread(target=lambda: os.system(f"python ./item/item.py")).start()
-  Thread(target=lambda: os.system(f"python ./order/order.py")).start()
-  Thread(target=lambda: os.system(f"python ./place_order/place_order.py")).start()
-  Thread(target=lambda: os.system(f"python ./stripe_wrapper/stripe_wrapper.py")).start()
-  Thread(target=lambda: os.system(f"python ./user/user.py")).start()
+  Thread(target=lambda: os.system("python ./email/email.py")).start()
+  Thread(target=lambda: os.system("python ./error/error.py")).start()
+  Thread(target=lambda: os.system("python ./item/item.py")).start()
+  Thread(target=lambda: os.system("python ./order/order.py")).start()
+  Thread(target=lambda: os.system("python ./place_order/place_order.py")).start()
+  Thread(target=lambda: os.system("python ./stripe_wrapper/stripe_wrapper.py")).start()
+  Thread(target=lambda: os.system("python ./user/user.py")).start()
