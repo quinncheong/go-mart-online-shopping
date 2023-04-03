6bb7524ce23290fc598e836f35c4bc26df4e5aeb
thejustinn <79741337+thejustinn@users.noreply.github.com>
2023-04-03 18:08:41 +0800

    added jwt to api calls in request header (Authorization)

--- UI/src/api/itemService.js
+++ UI/src/api/itemService.js
@@ -1,9 +1,10 @@
 import axios from "axios";
 
 var email="";
-if (localStorage.getItem("cognito-user-jwt")) {
+var token = localStorage.getItem("cognito-user-jwt");
+if (token) {
 	// local storage is available, set the value
-	email = JSON.parse(localStorage.getItem("cognito-user-jwt"));
+	email = JSON.parse(token);
 	if (email["Email"]) {
 		email = email["email"];
 	
@@ -26,31 +27,51 @@ const ITEM_URL =
 		: `${ITEM_BASEURL}/v1/item`;
 
 export const getAllItems = async () => {
-	const response = await axios.get(`${ITEM_URL}/all`);
+	const response = await axios.get(`${ITEM_URL}/all`, {
+		headers: {
+			'Authorization': token 
+		}
+	});
 	console.info(response);
 	return response.data;
 };
 
 export const getItemById = async (id) => {
-	const response = await axios.get(`${ITEM_URL}/${id}`);
+	const response = await axios.get(`${ITEM_URL}/${id}`, {
+		headers: {
+			'Authorization': token 
+		}
+	});
 	console.info(response);
 	return response.data;
 };
 
 export const getNumItems = async () => {
-	const response = await axios.get(`${ITEM_URL}/get-num-items`);
+	const response = await axios.get(`${ITEM_URL}/get-num-items`, {
+		headers: {
+			'Authorization': token 
+		}
+	});
 	console.info(response);
 	return response.data;
 };
 
 export const getRecommendedItems= async () => {
-	const response = await axios.get(`${PLACE_ORDER_URL}/displayItems/`+email);
+	const response = await axios.get(`${PLACE_ORDER_URL}/displayItems/`+email, {
+		headers: {
+			'Authorization': token 
+		}
+	});
 	console.info(response);
 	return response.data;
 };
 // POST APIS
 export const queryItemsByEsk = async ({ esk }) => {
-	const response = await axios.post(`${ITEM_URL}/esk`, { esk });
+	const response = await axios.post(`${ITEM_URL}/esk`, { esk }, {
+		headers: {
+			'Authorization': token 
+		}
+	});
 	console.info(response);
 	return response.data;
 };
