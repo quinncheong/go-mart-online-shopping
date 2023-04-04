44c9de1c21a888aa688bdaf8ab1c28db248a9b55
ranonsew <ranonsew.2020@scis.smu.edu.sg>
2023-04-04 21:26:56 +0800

    added encoded jwt storing

--- UI/src/api/cookie.js
+++ UI/src/api/cookie.js
@@ -10,9 +10,14 @@ export const getToken = (name="cognito-user-jwt") => {
   const token = JSON.parse(window.localStorage.getItem(name))
   if (!token || isExpired(token)) {
     setToken(name, null)
+    if (name === "cognito-user-jwt") {
+      setToken("cognito-encoded-jwt", null)
+    }
     console.log("token expired")
     return null
   } else {
     return token
   }
 }
+
+export const getRaw = (name="cognito-encoded-jwt") => JSON.parse(window.localStorage.getItem(name))
